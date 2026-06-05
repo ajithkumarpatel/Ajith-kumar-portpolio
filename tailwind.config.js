b98516ed97acd740829e480bc78f03e7a8189285
Ajith kumar <kumarpatelajith@gmail.com>
2026-06-05 22:41:10 +0530

    Simplify tailwind.config.js structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 export default {
   darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
